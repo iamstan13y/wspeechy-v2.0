a961a00e9421c5f1a5d5ed5ec90c66070f72bdfc
= <=>
2021-07-15 21:45:37 +0200

    Initial commit.

---
+++ Wspeechy 2.0/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Wspeechy_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrMsg = new System.Windows.Forms.Timer(this.components);
+            this.bunifuCards1 = new ns1.BunifuCards();
+            this.lblMsg = new ns1.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new ns1.BunifuCustomLabel();
+            this.btnAbout = new ns1.BunifuImageButton();
+            this.bunifuCustomLabel1 = new ns1.BunifuCustomLabel();
+            this.txtUser = new ns1.BunifuMetroTextbox();
+            this.btnSave = new ns1.BunifuThinButton2();
+            this.bunifuCards1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAbout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrMsg
+            // 
+            this.tmrMsg.Interval = 200;
+            this.tmrMsg.Tick += new System.EventHandler(this.tmrMsg_Tick);
+            // 
+            // bunifuCards1
+            // 
+            this.bunifuCards1.BackColor = System.Drawing.Color.Black;
+            this.bunifuCards1.BackgroundImage = global::Wspeechy_2._0.Properties.Resources.C__Data_Users_DefApps_AppData_INTERNETEXPLORER_Temp_Saved_Images_HxQ6Oa1;
+            this.bunifuCards1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuCards1.BorderRadius = 0;
+            this.bunifuCards1.BottomSahddow = false;
+            this.bunifuCards1.color = System.Drawing.Color.Red;
+            this.bunifuCards1.Controls.Add(this.lblMsg);
+            this.bunifuCards1.Controls.Add(this.bunifuCustomLabel2);
+            this.bunifuCards1.Controls.Add(this.btnAbout);
+            this.bunifuCards1.Controls.Add(this.bunifuCustomLabel1);
+            this.bunifuCards1.Controls.Add(this.txtUser);
+            this.bunifuCards1.Controls.Add(this.btnSave);
+            this.bunifuCards1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuCards1.LeftSahddow = false;
+            this.bunifuCards1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuCards1.Name = "bunifuCards1";
+            this.bunifuCards1.RightSahddow = true;
+            this.bunifuCards1.ShadowDepth = 20;
+            this.bunifuCards1.Size = new System.Drawing.Size(339, 213);
+            this.bunifuCards1.TabIndex = 3;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.BackColor = System.Drawing.Color.Transparent;
+            this.lblMsg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblMsg.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.Yellow;
+            this.lblMsg.Location = new System.Drawing.Point(46, 65);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(286, 20);
+            this.lblMsg.TabIndex = 6;
+            this.lblMsg.Text = "Enter Name for Welcome Message!";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.Gray;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(5, 10);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(97, 14);
+            this.bunifuCustomLabel2.TabIndex = 5;
+            this.bunifuCustomLabel2.Text = "&W&Speechy 2.0";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.Color.Transparent;
+            this.btnAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbout.Image = ((System.Drawing.Image)(resources.GetObject("btnAbout.Image")));
+            this.btnAbout.ImageActive = null;
+            this.btnAbout.Location = new System.Drawing.Point(290, 11);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(37, 30);
+            this.btnAbout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnAbout.TabIndex = 4;
+            this.btnAbout.TabStop = false;
+            this.btnAbout.Zoom = 10;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.Red;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(29, 95);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(94, 19);
+            this.bunifuCustomLabel1.TabIndex = 2;
+            this.bunifuCustomLabel1.Text = "USERNAME:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.Black;
+            this.txtUser.BorderColorFocused = System.Drawing.Color.Red;
+            this.txtUser.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtUser.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.txtUser.BorderThickness = 3;
+            this.txtUser.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUser.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.Color.Snow;
+            this.txtUser.isPassword = false;
+            this.txtUser.Location = new System.Drawing.Point(122, 89);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(201, 33);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // btnSave
+            // 
+            this.btnSave.ActiveBorderThickness = 1;
+            this.btnSave.ActiveCornerRadius = 20;
+            this.btnSave.ActiveFillColor = System.Drawing.Color.Red;
+            this.btnSave.ActiveForecolor = System.Drawing.Color.White;
+            this.btnSave.ActiveLineColor = System.Drawing.Color.Red;
+            this.btnSave.BackColor = System.Drawing.Color.Black;
+            this.btnSave.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSave.BackgroundImage")));
+            this.btnSave.ButtonText = "Save && Exit";
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.Red;
+            this.btnSave.IdleBorderThickness = 1;
+            this.btnSave.IdleCornerRadius = 20;
+            this.btnSave.IdleFillColor = System.Drawing.Color.Transparent;
+            this.btnSave.IdleForecolor = System.Drawing.Color.Red;
+            this.btnSave.IdleLineColor = System.Drawing.Color.Red;
+            this.btnSave.Location = new System.Drawing.Point(160, 131);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 47);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 213);
+            this.Controls.Add(this.bunifuCards1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bunifuCards1.ResumeLayout(false);
+            this.bunifuCards1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAbout)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ns1.BunifuMetroTextbox txtUser;
+        private ns1.BunifuThinButton2 btnSave;
+        private ns1.BunifuCustomLabel bunifuCustomLabel1;
+        private ns1.BunifuCards bunifuCards1;
+        private ns1.BunifuImageButton btnAbout;
+        private ns1.BunifuCustomLabel lblMsg;
+        private ns1.BunifuCustomLabel bunifuCustomLabel2;
+        private System.Windows.Forms.Timer tmrMsg;
+    }
+}
+
---
+++ Wspeechy 2.0/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using Wspeechy_2._0.Properties;
+using Speak;
+using System.IO;
+using Regedit;
+
+namespace Wspeechy_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string username;
+        NewReg reg = new NewReg();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Settings.Default.Username = "";
+            //Settings.Default.Save();
+            if (!(Settings.Default.Username == ""))
+            {
+                bunifuCards1.Visible = false;
+                string mytext = "";
+                SpeakLib speak = new SpeakLib();
+                DateTime time = DateTime.Now;
+                username = Settings.Default.Username;
+                int timeCnt = int.Parse(time.Hour.ToString());
+                
+
+                if (timeCnt < 12)
+                {
+                    mytext = "Good Morning " + username + "Hope You Had A Good Night!, How Are You Doing This Morning?";
+                }
+                else if (timeCnt >= 12 && timeCnt < 18)
+                {
+                    mytext = "Afternoon " + username + "How Is Your Day Going So Far?, Hope You Are Enjoying!";
+                }
+                else if (timeCnt >= 18 && timeCnt < 24)
+                {
+                    mytext = "Evening " + username + "How Was Your Day?, Hope You Enjoyed!, Have A Good Night In Advance!";
+                }
+
+                speak.Speak(mytext);
+                Application.Exit();
+
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                MessageBox.Show("Please Enter a Valid Username!", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else
+            {
+                username = txtUser.Text;
+                Settings.Default.Username = username;
+                Settings.Default.Save();
+                reg.Save(Application.ProductName, Application.ExecutablePath);
+                MessageBox.Show("Saved Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WSPEECHY 2.0 \n\nCopyright © 2019. All Rights Reserved \n\n Product of Stanton-Technologies", "About",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void tmrMsg_Tick(object sender, EventArgs e)
+        {
+            if (lblMsg.Visible == true)
+            {
+                lblMsg.Visible = false;
+            } else
+            {
+                lblMsg.Visible = true;
+            }
+
+        }
+
+    }
+}
